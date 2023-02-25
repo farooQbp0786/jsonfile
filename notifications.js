89061b357556d7a4d08d6fb7423b8c948106fe44
farooQbp0786 <51960293+farooQbp0786@users.noreply.github.com>
2023-02-25 19:47:05 +0530

    notifications

---
+++ notifications.js
@@ -0,0 +1,16 @@
+{
+    "notifications": [
+        {
+            "date": '11 - 06 - 2022',
+            "title": 'Detailed Time Tables of B.Tech S8 (R, S) (2015 scheme) June 2022 Examinations (including Part-Time) and B.Tech S8 Honours Exam June 2022 (2015 scheme)',
+            "desc": 'Detailed Time Tables of B.Tech S8 (R, S) (2015 scheme) June 2022 Examinations (including Part-Time) and B.Tech S8 Honours Exam June 2022 (2015 scheme)',
+            "link": 'https://mechktuassist.in/detailed-time-tables-of-b-tech-s8-r-s-2015-scheme-june-2022-examinations-including-part-time-and-b-tech-s8-honours-exam-june-2022-2015-scheme-notification/'
+        },
+        {
+            "date": '11 - 06 - 2022',
+            "title": 'Detailed Time Tables of B.Tech S8 (R, S) (2015 scheme) June 2022 Examinations (including Part-Time) and B.Tech S8 Honours Exam June 2022 (2015 scheme)',
+            "desc": 'Detailed Time Tables of B.Tech S8 (R, S) (2015 scheme) June 2022 Examinations (including Part-Time) and B.Tech S8 Honours Exam June 2022 (2015 scheme)',
+            "link": 'https://mechktuassist.in/detailed-time-tables-of-b-tech-s8-r-s-2015-scheme-june-2022-examinations-including-part-time-and-b-tech-s8-honours-exam-june-2022-2015-scheme-notification/'
+        }
+    ]
+}
